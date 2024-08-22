500603ab7a9a19d24d4a157bd6488a352018bfef
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2024-08-22 23:10:10 +0530

    Add files via upload

---
+++ 02_variables.js
@@ -0,0 +1,12 @@
+// Introduction to variables
+
+let a = "car" // here 'a' is variable name which holds the data "car" in it
+let b = 85 // here 'b' is variable name which holds the data 85 in it
+
+var c = "bike"
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+// let 8name = "arun" // throws an error as it violates the variable name condition, the variable name should not start with a number.
