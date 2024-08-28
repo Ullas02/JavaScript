ee97e0a4de3639926f493530f479b52748a3191c
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2024-08-28 18:06:11 +0530

    Add files via upload

---
+++ 10_while_dowhile_loop.js
@@ -0,0 +1,15 @@
+// while loop (pre-tested loop)
+
+let i = 0;
+while (i < 10) {
+    console.log("Value of i : " + i);
+    i++;
+}
+console.log("\n");
+// do-while loop (post-tested loop)
+
+let j = 0;
+do {
+    console.log("Value of j : " + j);
+    j++;
+} while (j <= 5);
