2a3aa9af9f3e38e58c340ec4cda4b6dc2a8615a6
Ullas02 <166708206+Ullas02@users.noreply.github.com>
2024-08-29 17:38:57 +0530

    Add files via upload

---
+++ 13_strings.js
@@ -0,0 +1,14 @@
+// strings in JS
+
+let name2 = "Joesph"
+console.log("Name : " + name2);
+
+// template literals (String intepolation : we can insert variables directly in template literal) , it has backtik.
+
+let frnd = `My name is ${name2}`;
+console.log(frnd);
+
+// escape sequence characters (\n \t \r)
+
+let fruit = 'bana\'na';
+console.log(fruit)
